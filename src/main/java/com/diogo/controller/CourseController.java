5ce345a921c5f3d633d19a52726c68f728ae6d66
Diogo Otoni <diogootoni@hotmail.com>
2023-08-29 16:10:33 -0300

    Implementacao do DTO e MAPPER

--- src/main/java/com/diogo/controller/CourseController.java
+++ src/main/java/com/diogo/controller/CourseController.java
@@ -3,7 +3,6 @@ package com.diogo.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.diogo.model.Course;
+import com.diogo.dto.CourseDTO;
 import com.diogo.service.CourseService;
 
 import jakarta.validation.Valid;
@@ -37,23 +36,24 @@ public class CourseController {
     }
 
     @GetMapping
-    public @ResponseBody List<Course> list(){
+    public @ResponseBody List<CourseDTO> list(){
         return courseService.list();
     }
 
     @GetMapping("/{id}")
-    public Course findById(@PathVariable @NotNull @Positive Long id){
+    public CourseDTO findById(@PathVariable @NotNull @Positive Long id){
         return courseService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course){
+    public CourseDTO create(@RequestBody @Valid @NotNull CourseDTO course){
         return courseService.create(course);
     }
 
     @PutMapping("/{id}")
-    public Course update(@PathVariable @NotNull @Positive Long id, @RequestBody Course course) {
+    public CourseDTO update(@PathVariable @NotNull @Positive Long id, 
+    @RequestBody @Valid @NotNull CourseDTO course) {
         return courseService.update(id, course);
     }
 
