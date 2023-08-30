fd4daacc7f97871c946f8fb009a73bc47d342d07
Diogo Otoni <diogootoni@hotmail.com>
2023-08-30 16:58:37 -0300

    implementacao de enums para category e status

--- src/main/java/com/diogo/model/Course.java
+++ src/main/java/com/diogo/model/Course.java
@@ -4,16 +4,20 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
+import com.diogo.enums.Category;
+import com.diogo.enums.Status;
+import com.diogo.enums.converters.CategoryConverter;
+import com.diogo.enums.converters.StatusConverter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -35,16 +39,14 @@ public class Course {
 
 
     @NotNull
-    @Length(max = 10)
-    @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 12, nullable = false)
-    private String category;
+    @Convert(converter = CategoryConverter.class)
+    private Category category;
 
     @NotNull
-    @Length(max = 10)
-    @Pattern(regexp = "Ativo|Inativo")
     @Column(length = 10, nullable = false)
-    private String status = "Ativo";
+    @Convert(converter = StatusConverter.class)
+    private Status status = Status.ACTIVE;
 
     
 }
